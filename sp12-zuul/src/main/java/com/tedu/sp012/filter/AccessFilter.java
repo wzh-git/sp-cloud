8db1b37ac16cede6619647b65972079b33fa98b0
hh <hh@DESKTOP-I99O0H9>
2019-06-28 10:23:05 +0800

    第一次

--- sp12-zuul/src/main/java/com/tedu/sp012/filter/AccessFilter.java
+++ sp12-zuul/src/main/java/com/tedu/sp012/filter/AccessFilter.java
@@ -15,8 +15,7 @@ public class AccessFilter extends ZuulFilter{
 	@Override
 	public boolean shouldFilter() {
 	    //对指定的serviceid过滤，如果要过滤所有服务，直接返回 true
-	    //12353436457
-		//235345464
+	    
 		RequestContext ctx = RequestContext.getCurrentContext();
 		String serviceId = (String) ctx.get(FilterConstants.SERVICE_ID_KEY);
 		if(serviceId.equals("item-service")) {
